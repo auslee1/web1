18a87b0e0ef1aa0a14cb6fb61b35ac8a96f18edf
auslee1 <167812699+auslee1@users.noreply.github.com>
2024-04-22 14:56:43 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-let myCar = [];
 
 function preload(){
   img1 = loadImage("resources/media/piza.jpeg")
@@ -7,16 +6,13 @@ function preload(){
 function setup() {
 createCanvas(800, 800);
   
-//myCar = new Car[10];
-
-for (let i = 0; i < 10; i++) {
-  myCar[i] = new Car(color(255,0,0),random(width),random(height), i);
-}
 }
 
 function draw() {
+  imageMode(CENTER);
+  noCursor();
   background(200,225,255);
-  //image(img1,mouseX,mouseY,200,200);
+  image(img1,mouseX,mouseY,200,200);
   
  
 }
